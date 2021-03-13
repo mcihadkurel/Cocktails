3d4b0c8970c58f49640627b3e8eaad3cc7d6ad2b
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2021-03-13 23:32:04 +0100

    Updates

---
+++ precache-manifest.434ec6b9d2d85a2952d79d8980328688.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e06789d1dd8c5e7b179a9cc814a3710",
+    "url": "/Cocktails/index.html"
+  },
+  {
+    "revision": "86e646181bd538ef13a6",
+    "url": "/Cocktails/static/css/main.e83e8772.chunk.css"
+  },
+  {
+    "revision": "e1f6309a6f0d3e350015",
+    "url": "/Cocktails/static/js/2.72303b8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Cocktails/static/js/2.72303b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86e646181bd538ef13a6",
+    "url": "/Cocktails/static/js/main.76f2e183.chunk.js"
+  },
+  {
+    "revision": "86297710c6556ae85886",
+    "url": "/Cocktails/static/js/runtime-main.df7651c9.js"
+  },
+  {
+    "revision": "f073f44ba639b3f45519878b8e0b4c3c",
+    "url": "/Cocktails/static/media/logo.f073f44b.png"
+  }
+]);
